ab9895a10d08569b0cdd49fcdc2ed980415e2f36
Brian <brianmkcebm@gmail.com>
2021-06-10 10:40:39 +0000

    first-commit-cholesterol

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -57,6 +57,22 @@ study = StudyDefinition(
             "incidence": 0.1,
         },
     ),
+   had_cholesterol_event=patients.with_these_clinical_events(
+        codelist=cholesterol_codelist,
+        between=BETWEEN,
+        returning="binary_flag",
+        return_expectations={"incidence": 0.1},
+    ),
+    # What clinical event was it?
+    cholesterol_event_code=patients.with_these_clinical_events(
+        codelist=cholesterol_codelist,
+        between=BETWEEN,
+        returning="code",
+        return_expectations={
+            "category": {"ratios": {x: 1 / len(cholesterol_codelist) for x in cholesterol_codelist}},
+            "incidence": 0.5,
+        },
+    )
 )
 
 # This is where we define measures using the study population. A measure is
@@ -65,6 +81,12 @@ measures = [
     Measure(
         id="sbp_by_practice",
         numerator="had_sbp_event",
+        denominator="population",
+        group_by=["practice"],
+    ),
+    Measure(
+        id="cholesterol_by_practice",
+        numerator="had_cholesterol_event",
         denominator="population",
         group_by=["practice"],
     ),
